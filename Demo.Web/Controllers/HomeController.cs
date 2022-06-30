4ef622c9155567a227df5ebbb2ddf489adc1f006
Revotadas <rafael.lopes24@gmail.com>
2022-06-30 15:06:22 +0100

    Create About View

--- Demo.Web/Controllers/HomeController.cs
+++ Demo.Web/Controllers/HomeController.cs
@@ -18,6 +18,10 @@ namespace Demo.Web.Controllers {
             return View();
         }
 
+        public IActionResult About() {
+            return View();
+        }
+
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error() {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });

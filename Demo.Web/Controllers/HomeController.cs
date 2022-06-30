cb6779f6cc631d3b0f6b68675446f5edfa116418
Revotadas <rafael.lopes24@gmail.com>
2022-06-30 15:29:48 +0100

    Create Xpto View

--- Demo.Web/Controllers/HomeController.cs
+++ Demo.Web/Controllers/HomeController.cs
@@ -22,6 +22,10 @@ namespace Demo.Web.Controllers {
             return View();
         }
 
+        public IActionResult Xpto() {
+            return View();
+        }
+
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error() {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
